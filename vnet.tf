b096206c6831a014dc22238f86070e82a408dd35
chaitanya <chaitanyapanja987@gmail.com>
2024-07-31 13:35:00 -0500

    added vnet-4

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "VNET3" {
-  name                = "VNET-3"
+  name                = "VNET-4"
   address_space       = ["10.40.0.0/16"]
   location            = "eastus"
   resource_group_name = "rg2"
