c08409de0702b3049fc35d3b4274ff7503d20f98
chaitanya <chaitanyapanja987@gmail.com>
2024-07-31 13:03:53 -0500

    second commit added 3 rg

--- rg.tf
+++ rg.tf
@@ -2,3 +2,15 @@ resource "azurerm_resource_group" "rg-1" {
   location = "eastus"
   name     = rg1
 }
+resource "azurerm_resource_group" "rg-2" {
+  location = "eastus"
+  name     = rg2
+}
+resource "azurerm_resource_group" "rg-3" {
+  location = "eastus"
+  name     = rg3
+}
+resource "azurerm_resource_group" "rg-4" {
+  location = "eastus"
+  name     = rg4
+}
